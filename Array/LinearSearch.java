6cb342a436afcf67e1156509267dbb70574e4df2
Bhumika Patil <bhumikapatil2204@gmail.com>
2024-10-28 15:17:22 +0530

    Added file linear search

---
+++ Array/LinearSearch.java
@@ -0,0 +1,28 @@
+public class LinearSearch{
+
+    public static int LinearSearch(int[] arr,int key)
+    {
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]==key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+       int[] arr={2,3,5,6,10,7,11}; 
+       int key=7;
+
+      int index= LinearSearch(arr,key);
+        if(index==-1)
+        {
+            System.err.println("Key does not exist");
+        }
+        else{
+            System.err.println("Key exist at index: "+index);
+        
+        }
+    }
+}
